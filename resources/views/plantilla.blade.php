63d8c5e7ba85fa0072de7d87c77090798ae9b488
unknown <SUITE@SUITESM708-ST28.cibertec.edu.pe>
2019-06-21 17:03:14 -0500

    validate update

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -18,16 +18,31 @@
         <form action="{{ route('notas.crear') }}" method="POST" class="pb-3">
           @csrf
 
-          @erro('nombre')
+          @error('nombre')
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+              <p style="margin:0px;">El nombre es obligatorio</p>
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+          @enderror
 
-          @enderro
+          @error('description')
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+              <p style="margin: 0px;">Es importante la descripcion!</p>
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+          @enderror
+
 
           <div class="row">
             <div class="col-md-3">
-              <input type="text" name="nombre" class="form-control" placeholder="Nombre">
+            <input type="text" name="nombre" class="form-control" placeholder="Nombre" value="{{ old('nombre')}}">
             </div>
             <div class="col-md-7">
-              <input type="text" name="description" class="form-control" placeholder="Description">
+            <input type="text" name="description" class="form-control" placeholder="Description" value="{{ old('description') }}">
             </div>
             <div class="col-md-2">
               <button class="btn btn-primary btn-block" type="submit">Agregar</button>
